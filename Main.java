1fe858a2b785501bc7ce746eeec2df4219b0cde8
krissstiina <162792967+krissstiina@users.noreply.github.com>
2024-11-29 10:36:18 +0300

    change 1

--- Main.java
+++ Main.java
@@ -1,4 +1,6 @@
 class Main{
     public static void main(String[] args) {
-        System.out.print("Это добавлено в ветке main"); }
+        System.out.print("Это добавлено в ветке main");
+        System.out.print("Изменение 1");
+    }
 }
