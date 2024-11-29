d5d4a0b8d5e3712c0491a1372847816308be300c
krissstiina <162792967+krissstiina@users.noreply.github.com>
2024-11-29 11:29:33 +0300

    add change 2 from dev

--- Main.java
+++ Main.java
@@ -2,5 +2,6 @@ class Main{
     public static void main(String[] args) {
         System.out.print("Это добавлено в ветке dev");
         System.out.print("Изменение 1");
+        System.out.print("Изменение 2");
     }
 }
